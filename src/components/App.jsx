f2cea2cc28f2639d6394e8cb29288224e536c1bd
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-14 16:01:40 +1100

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+//CHALLENGE: Make the code in App.jsx work.
+//The final app should have a single contact
+//with fName, lName and email.
+
+//HINT: You'll need to apply the following things you learnt:
+//1. Using JS Objects with state.
+//2. Making use of previous state when changing state.
+//3. Working with forms in React.
+//4. Handing events
 
 function App() {
   const [contact, setContact] = useState({
@@ -7,6 +16,20 @@ function App() {
     email: ""
   });
 
+  function handleContact(event) {
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+    const { name, value } = event.target;
+    // console.log(name);
+    if (name === "fName") {
+      setContact({ fName: value, lName: contact.lName, email: contact.email });
+    } else if (name === "lName") {
+      setContact({ fName: contact.fName, lName: value, email: contact.email });
+    } else if (name === "email") {
+      setContact({ fName: contact.fName, lName: contact.lName, email: value });
+    }
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,9 +37,24 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input
+          onChange={handleContact}
+          name="fName"
+          placeholder="First Name"
+          value={contact.fName}
+        />
+        <input
+          onChange={handleContact}
+          name="lName"
+          placeholder="Last Name"
+          value={contact.lName}
+        />
+        <input
+          onChange={handleContact}
+          name="email"
+          placeholder="Email"
+          value={contact.email}
+        />
         <button>Submit</button>
       </form>
     </div>
